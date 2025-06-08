e563855b96bde68a29ced2318c610e8b37da866b
Tamsin ROUAUD <tamsinrouaud83@gmail.com>
2025-06-08 21:09:23 +0200

    Page d'accueil pratiquement finalisée reste à faire page panier et mon compte

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,24 @@
 <?php
 
+// src/Controller/HomeController.php
+
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findBy([], ['id' => 'DESC'], 9); 
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $products,
         ]);
     }
 }
+
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
-    {
-        return $this->render('product/index.html.twig', [
-            'app_product' => 'ProductController',
-        ]);
-    }
+  #[Route('/products', name: 'app_products')]
+public function index(ProductRepository $productRepository): Response
+{
+    $products = $productRepository->findBy([], ['id' => 'DESC']);
+
+    return $this->render('product/productList.html.twig', [
+        'products' => $products,
+    ]);
+}
+
+#[Route('/product/{id}', name: 'app_product')]
+public function show(Product $product): Response
+{
+    return $this->render('product/productDetail.html.twig', [
+        'product' => $product,
+    ]);
+}
+
+
 }
